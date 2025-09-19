82aa4751444633b46fcb258e02c76124abae101e
dkelll <dkelll@users.noreply.github.com>
2025-09-19 14:39:06 -0500

    wip: Plugin dev

--- lua/dkel/lazy/sf.lua
+++ lua/dkel/lazy/sf.lua
@@ -4,14 +4,13 @@ return {
         enabled = false,
 >>>>>>> Stashed changes
         "sf",
-        dir = "~/projects/plugins/sf.nvim",
+        dir = "~/Projects/nvim-plugins/telescope-salesforce.nvim",
         dependencies = {
             "nvim-telescope/telescope.nvim",
             "nvim-lua/plenary.nvim",
         },
         config = function()
             local sf = require("sf")
-            sf.setup()
             vim.keymap.set("n", "<leader>sf", function() sf.toggle_window() end)
         end
 }
