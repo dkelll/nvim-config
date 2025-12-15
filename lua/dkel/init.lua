9bbbeccd92e60902d330381fb8a69dbdbec3d8e9
dkelll <dkelll@users.noreply.github.com>
2025-12-14 22:29:44 -0600

    feat: Overhauling

--- lua/dkel/init.lua
+++ lua/dkel/init.lua
@@ -63,4 +63,5 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 vim.g.netrw_list_hide = vim.fn['netrw_gitignore#Hide']()
-
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_node_provider = 0
