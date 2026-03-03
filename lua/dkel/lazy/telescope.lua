1b6a3df4c7666fc04ddcaa1956b4ea46bd0c4eb0
dkelll <dkelll@users.noreply.github.com>
2026-03-02 18:44:26 -0600

    feat: Show hidden files

--- lua/dkel/lazy/telescope.lua
+++ lua/dkel/lazy/telescope.lua
@@ -21,7 +21,7 @@ return {
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-        vim.keymap.set('n', '<leader>tf', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>tf', function() builtin.find_files({ hidden = true }) end)
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>ts', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
